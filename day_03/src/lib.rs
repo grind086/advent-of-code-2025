c5a1cf89497b2d857331eac2bb67adb6843568f8
Rob Grindeland <rgrindeland@gmail.com>
2025-12-05 21:55:40 -0600

    day 3 part 2

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -7,25 +7,33 @@ pub static TEST_INPUT: &str = concat!(
     "818181911112111\n",
 );
 
-pub fn max_joltage_sum(input: &str) -> u32 {
-    iter_bank_max_joltages(input).sum()
+pub fn max_joltage_sum(input: &str, size: usize) -> u64 {
+    iter_bank_max_joltages(input, size).sum()
 }
 
-pub fn iter_bank_max_joltages(input: &str) -> impl Iterator<Item = u32> {
-    input.lines().map(bank_max_joltage)
+pub fn iter_bank_max_joltages(input: &str, size: usize) -> impl Iterator<Item = u64> {
+    input
+        .lines()
+        .map(move |bank| bank_max_joltage(bank.trim().as_bytes(), size))
 }
 
-pub fn bank_max_joltage(bank: &str) -> u32 {
-    let bank = bank.trim().as_bytes();
-    let (i, i_val) = scan_max(bank);
-    let (a, b) = if i == bank.len() - 1 {
-        let (_, j_val) = scan_max(&bank[0..i]);
-        (j_val, i_val)
-    } else {
-        let (_, j_val) = scan_max(&bank[i + 1..]);
-        (i_val, j_val)
-    };
-    10 * (a - b'0') as u32 + (b - b'0') as u32
+pub fn bank_max_joltage(bank: &[u8], size: usize) -> u64 {
+    fn inner(bank: &[u8], size: usize, clamp_max: Option<usize>) -> u64 {
+        if size == 0 {
+            return 0;
+        }
+
+        let scan_max_index = clamp_max.unwrap_or(bank.len());
+        let (i, i_val) = scan_max(&bank[0..scan_max_index]);
+        if i + size <= bank.len() {
+            (i_val - b'0') as u64 * 10u64.pow((size - 1) as _)
+                + inner(&bank[i + 1..], size - 1, None)
+        } else {
+            inner(bank, size, Some(i))
+        }
+    }
+
+    inner(bank, size, None)
 }
 
 fn scan_max(bytes: &[u8]) -> (usize, u8) {
@@ -48,18 +56,31 @@ mod tests {
     #[test]
     fn part1_test_input_max_joltages() {
         assert_eq!(
-            iter_bank_max_joltages(TEST_INPUT).collect::<Vec<_>>(),
+            iter_bank_max_joltages(TEST_INPUT, 2).collect::<Vec<_>>(),
             vec![98, 89, 78, 92]
         );
     }
 
     #[test]
     fn part1_test_input_max_joltage() {
-        assert_eq!(max_joltage_sum(TEST_INPUT), 357);
+        assert_eq!(max_joltage_sum(TEST_INPUT, 2), 357);
     }
 
     #[test]
     fn max_digit_can_repeat() {
-        assert_eq!(bank_max_joltage("124444266221211"), 66);
+        assert_eq!(bank_max_joltage(b"124444266221211", 2), 66);
+    }
+
+    #[test]
+    fn part2_test_input_max_joltages() {
+        assert_eq!(
+            iter_bank_max_joltages(TEST_INPUT, 12).collect::<Vec<_>>(),
+            vec![987654321111, 811111111119, 434234234278, 888911112111]
+        );
+    }
+
+    #[test]
+    fn part2_test_input_max_joltage() {
+        assert_eq!(max_joltage_sum(TEST_INPUT, 12), 3121910778619);
     }
 }
--- day_03/src/part1.rs
+++ day_03/src/part1.rs
@@ -1,5 +1,5 @@
 use day_03::*;
 
 fn main() {
-    println!("{}", max_joltage_sum(INPUT))
+    println!("{}", max_joltage_sum(INPUT, 2))
 }
