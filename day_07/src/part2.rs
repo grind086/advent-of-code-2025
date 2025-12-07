03ebe5f130caa2f9a30b86e3ed30e273d3c5885b
Rob Grindeland <rgrindeland@gmail.com>
2025-12-07 01:06:31 -0600

    day 7 part 2

--- day_07/src/part2.rs
+++ day_07/src/part2.rs
@@ -1,3 +1,38 @@
 use day_07::*;
 
-fn main() {}
+fn main() {
+    let mut lines = INPUT.lines();
+
+    let init = lines.next().unwrap();
+    let mut state: Vec<u64> = init
+        .trim()
+        .as_bytes()
+        .iter()
+        .map(|b| match *b {
+            b'S' => 1,
+            b'.' => 0,
+            _ => panic!("Only S and . in first line"),
+        })
+        .collect();
+
+    let mut splits = 0;
+    for line in lines {
+        for (i, b) in line.trim().as_bytes().iter().enumerate() {
+            match *b {
+                b'.' => {}
+                b'^' => {
+                    if state[i] != 0 {
+                        state[i + 1] += state[i];
+                        state[i - 1] += state[i];
+                        state[i] = 0;
+                        splits += 1;
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    println!("{splits}");
+    println!("{}", state.iter().copied().sum::<u64>());
+}
